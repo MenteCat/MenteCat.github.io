d62ba704cc9e6e459c4bf25dd5ec43ffea7e6b9e
Tina Iuso <tinaiuso@gmail.com>
2021-04-28 14:22:10 +0100

    js for the hamburger added

--- js/index.js
+++ js/index.js
@@ -23,3 +23,18 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+// hamburger ---->
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
