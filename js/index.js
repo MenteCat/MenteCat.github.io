01518f98a6653676f36b23f597c833a6a4eb3257
Tina Iuso <tinaiuso@gmail.com>
2021-05-18 21:39:56 +0100

    js notifyEmail function added

--- js/index.js
+++ js/index.js
@@ -14,13 +14,13 @@ navLinks.forEach((link) => {
 });
 
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -37,4 +37,8 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+};
+
+const notifyEmail = () => {
+  document.getElementById("send-btn").value = "Message Sent";
 };
